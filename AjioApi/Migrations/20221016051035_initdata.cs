c248a8309eac5e1c31e5484ebcd180097a42551f
Shrikanth-p <shrikanthchoudary05@gmail.com>
2022-10-16 11:26:42 +0530

    Add project files.

---
+++ AjioApi/Migrations/20221016051035_initdata.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AjioApi.Migrations
+{
+    public partial class initdata : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Registration",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    username = table.Column<string>(type: "varchar(25)", unicode: false, maxLength: 25, nullable: true),
+                    useremail = table.Column<string>(type: "varchar(40)", unicode: false, maxLength: 40, nullable: true),
+                    password = table.Column<string>(type: "varchar(25)", unicode: false, maxLength: 25, nullable: true),
+                    mobile = table.Column<string>(type: "varchar(25)", unicode: false, maxLength: 25, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Registration", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Registration");
+        }
+    }
+}
---
+++ AjioApi/Models/Registration.cs
@@ -0,0 +1,16 @@
+﻿using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AjioApi.Models
+{
+    public partial class Registration
+    {
+        public int Id { get; set; }
+        public string? Username { get; set; }
+        public string? Useremail { get; set; }
+        public string? Password { get; set; }
+        public string? Mobile { get; set; }
+    }
+}
